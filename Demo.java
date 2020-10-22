e18738170ea8701c3128ea1abc11d9f3ec2d5c16
acho20 <acho20@stuy.edu>
2020-10-21 21:50:04 -0400

    I tested this program and it works now

--- Demo.java
+++ Demo.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Demo{
  public static void printLoop(int x){
   for (int i = x; i > 0; i--){
@@ -8,6 +10,61 @@ public class Demo{
   }    
  } 
 
+ public static String arrToString(int[] arr){
+  String x = "{";
+  for(int i = 0; i < arr.length; i++){
+   x += Integer.toString(arr[i]);
+   if (i == arr.length - 1){
+    x +="}";
+   }else{
+    x +=", ";
+   }
+  }
+  if(arr.length == 0){
+   return "{}";
+  }
+  return x;
+ }
+
+ public static String arrayDeepToString(int[][]arr){
+  String x = "{";
+  for(int i = 0; i < arr.length; i++){
+   x += arrToString(arr[i]);
+   if (i == arr.length - 1){
+    x +="}";
+   }else{
+    x +=", ";
+   }
+  }
+  return x;
+ }
+
+ public static int[][] create2DArray(int rows, int cols, int maxValue){
+  int[][] x = new int[rows][cols];
+  Random rand = new Random();
+  for (int i = 0; i < x.length; i++){
+   for (int j = 0; j < x[0].length; j++){
+    x[i][j] = rand.nextInt(maxValue+1);
+   }
+  }
+  return x;
+ }
+
+ public static int[][] create2DArrayRandomized(int rows, int cols, int maxValue){
+  int[][] x = new int[rows][];
+  Random rand = new Random();
+  for(int i = 0; i < x.length; i++){
+   x[i] = new int[rand.nextInt(cols + 1)];
+  }
+  for (int i = 0; i < x.length; i++){
+   for (int j = 0; j < x[i].length; j++){
+    x[i][j] = rand.nextInt(maxValue+1);
+   }
+  }
+  return x;
+ }
+
+
 
  public static void main(String[] args){
   if (args.length == 1){
@@ -16,4 +73,6 @@ public class Demo{
    printLoop(5);
   }  
  }
+
 }
+
